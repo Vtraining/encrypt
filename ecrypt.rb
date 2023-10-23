bd6968e2366cd773ea92797883b2af970480149a
Vtraining <40.s.a.m.y.40@gmail.com>
2023-10-23 20:53:40 +0300

    Add new method of encryption (SHA2)

--- ecrypt.rb
+++ ecrypt.rb
@@ -6,11 +6,12 @@ string = STDIN.gets.chomp
 puts "With what method do you want to enrypt your phrase:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 
 encrypt_method = STDIN.gets.to_i
 
-until encrypt_method.between?(1, 2)
-  puts "Please choose 1 or 2"
+until encrypt_method.between?(1, 3)
+  puts "Please choose 1, 2 or 3"
   encrypt_method = STDIN.gets.to_i
 end
 
@@ -19,5 +20,6 @@ puts "That is the result:"
 case encrypt_method
 when 1 then puts Digest::MD5.hexdigest(string)
 when 2 then puts Digest::SHA1.hexdigest(string)
+when 3 then puts Digest::SHA2.hexdigest(string)
 end
 
